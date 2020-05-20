115134f2d3b6347c0b1891ed5379caddbea47252
saurabhgupta-test <65661485+saurabhgupta-test@users.noreply.github.com>
2020-05-20 18:33:32 +0530

    Update Example_and_or.java

--- Example_and_or.java
+++ Example_and_or.java
@@ -4,6 +4,7 @@ public class Example_and_or
 {
 	public static void main(String[] args) 
 	{
-		 System.out.println(" git repo 2 ");
+		 System.out.println(" git repo 13 ");
+		System.out.println(" change from git Hub ");
 	 }
 }
